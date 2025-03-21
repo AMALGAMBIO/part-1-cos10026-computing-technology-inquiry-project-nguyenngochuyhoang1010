9d2bec75b35f6d4899c871742c55ab2aed757ec9
CaoVietAnh2 <caovietanh2799@gmail.com>
2025-03-21 17:05:43 +0700

    Update apply.php

--- project2/apply.php
+++ project2/apply.php
@@ -1,4 +1,3 @@
-
 <?php include 'menu.php'; ?>
 <link rel="stylesheet" href="styles/styleapply.css">
 <div class="container">
@@ -8,17 +7,17 @@
     <form id="application" method="post" action="process_eoi.php" novalidate="novalidate">     
         <fieldset class="form-section">
             <legend>Job Reference Number</legend>
-            <input type="text" name="JobRef" class="input-field" required>
+            <input type="text" name="JobRef" class="input-field" maxlength="5" required>
         </fieldset>
 
         <fieldset class="form-section">
             <legend>Personal Information</legend>
             <label>First Name: <input type="text" name="FirstName" maxlength="20" class="input-field" required></label>
             <label>Last Name: <input type="text" name="LastName" maxlength="20" class="input-field" required></label>
-            <label>Address: <input type="text" name="Address" maxlength="40" class="input-field" required></label>
-            <label>Suburb/Town: <input type="text" name="Suburb" maxlength="40" class="input-field" required></label>
+            <label>Street Address: <input type="text" name="StreetAddress" maxlength="40" class="input-field" required></label>
+            <label>Suburb/Town: <input type="text" name="SuburbTown" maxlength="40" class="input-field" required></label>
             <label>State:
-                <select name="State" class="input-field">
+                <select name="State" class="input-field" required>
                     <option value="VIC">VIC</option>
                     <option value="NSW">NSW</option>
                     <option value="QLD">QLD</option>
@@ -29,9 +28,9 @@
                     <option value="ACT">ACT</option>
                 </select>
             </label>
-            <label>Postcode: <input type="text" name="Postcode" class="input-field" required></label>
+            <label>Postcode: <input type="text" name="Postcode" maxlength="4" class="input-field" required></label>
             <label>Email: <input type="email" name="Email" class="input-field" required></label>
-            <label>Phone Number: <input type="text" name="Phone" class="input-field" required></label>
+            <label>Phone Number: <input type="text" name="Phone" maxlength="12" class="input-field" required></label>
         </fieldset>
 
         <fieldset class="form-section">
@@ -39,7 +38,7 @@
             <label><input type="checkbox" name="Skill1" value="HTML"> HTML</label>
             <label><input type="checkbox" name="Skill2" value="CSS"> CSS</label>
             <label><input type="checkbox" name="Skill3" value="JavaScript"> JavaScript</label>
-            <label>Other Skills: <input type="text" name="OtherSkills" class="input-field"></label>
+            <label>Other Skills: <textarea name="OtherSkills" class="input-field"></textarea></label>
         </fieldset>
 
         <button type="submit" class="submit-btn">Submit Application</button>
