c653f80d003111e671a8e74a79cc17c0c40abf1c
NamBuiThe <nambuithe612@gmail.com>
2025-03-22 19:04:36 +0700

    Update apply.php

--- project2/apply.php
+++ project2/apply.php
@@ -4,43 +4,59 @@
     <h1 class="form-title" text-align="right">Job Application Form</h1>
     <p class="form-subtitle">Please fill out the form below to apply for a position with us.</p>
 
-    <form id="application" method="post" action="process_eoi.php" novalidate="novalidate">     
-        <fieldset class="form-section">
-            <legend>Job Reference Number</legend>
-            <input type="text" name="JobRef" class="input-field" maxlength="10" required>
+    <form id="application" method="post" action="process_eoi.php" novalidate="novalidate">
+        
+    <fieldset class="form-group">
+    <legend>Job Reference Number</legend>
+            <input type="text" id="job_reference" name="job_reference" required>
+    </fieldset>
+
+    <fieldset class="form-group">
+        <legend>Personal Information</legend>
+            <label for="first_name">First Name: <input type="text" id="first_name" name="first_name" maxlength="20" required></label>
+
+            <label for="last_name">Last Name: <input type="text" id="last_name" name="last_name" maxlength="20" required></label>
+
+            <label for="street_address">Street Address: <input type="text" id="street_address" name="street_address" maxlength="40" required></label>
+
+            <label for="suburb_town">Suburb/Town: <input type="text" id="suburb_town" name="suburb_town" maxlength="40" required></label>
+
+            <label for="state">State:</label>
+            <select id="state" name="state" required>
+                <option value="">Select State</option>
+                <option value="VIC">VIC</option>
+                <option value="NSW">NSW</option>
+                <option value="QLD">QLD</option>
+                <option value="NT">NT</option>
+                <option value="WA">WA</option>
+                <option value="SA">SA</option>
+                <option value="TAS">TAS</option>
+                <option value="ACT">ACT</option>
+            </select>
+
+            <label for="postcode">Postcode: <input type="text" id="postcode" name="postcode" pattern="\d{4}" required></label>
+
+            <label for="email">Email Address: <input type="email" id="email" name="email" required></label>
+
+            <label for="phone">Phone Number: <input type="text" id="phone" name="phone" pattern="\d{10}" required></label>
+    </fieldset>
+
+        <fieldset class="form-group checkbox-group">
+            <legend>Skills</legend><br>
+            <input type="checkbox" id="skill1" name="Skill1" value="HTML">
+            <label for="skill1">HTML</label><br>
+
+            <input type="checkbox" id="skill2" name="Skill2" value="CSS">
+            <label for="skill2">CSS</label><br>
+
+            <input type="checkbox" id="skill3" name="Skill3" value="JavaScript">
+            <label for="skill3">JavaScript</label><br>
         </fieldset>
 
-        <fieldset class="form-section">
-            <legend>Personal Information</legend>
-            <label>First Name: <input type="text" name="FirstName" maxlength="20" class="input-field" required></label>
-            <label>Last Name: <input type="text" name="LastName" maxlength="20" class="input-field" required></label>
-            <label>Street Address: <input type="text" name="StreetAddress" maxlength="40" class="input-field" required></label>
-            <label>Suburb/Town: <input type="text" name="SuburbTown" maxlength="40" class="input-field" required></label>
-            <label>State:
-                <select name="State" class="input-field" required>
-                    <option value="VIC">VIC</option>
-                    <option value="NSW">NSW</option>
-                    <option value="QLD">QLD</option>
-                    <option value="NT">NT</option>
-                    <option value="WA">WA</option>
-                    <option value="SA">SA</option>
-                    <option value="TAS">TAS</option>
-                    <option value="ACT">ACT</option>
-                </select>
-            </label>
-            <label>Postcode: <input type="text" name="Postcode" maxlength="5" class="input-field" required></label>
-            <label>Email: <input type="email" name="Email" class="input-field" required></label>
-            <label>Phone Number: <input type="text" name="Phone" maxlength="12" class="input-field" required></label>
+        <fieldset class="form-group">
+            <label for="other_skills">Other Skills:</label>
+            <textarea id="other_skills" name="other_skills" rows="3"></textarea>
         </fieldset>
-
-        <fieldset class="form-section">
-            <legend>Skills</legend>
-            <label><input type="checkbox" name="Skill1" value="HTML"> HTML</label>
-            <label><input type="checkbox" name="Skill2" value="CSS"> CSS</label>
-            <label><input type="checkbox" name="Skill3" value="JavaScript"> JavaScript</label>
-            <label>Other Skills: <textarea name="OtherSkills" class="input-field"></textarea></label>
-        </fieldset>
-
         <button type="submit" class="submit-btn">Submit Application</button>
     </form>
 </div>
