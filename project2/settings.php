4f1acf5ebab9d4da996be853232936acbdf8f7a7
CaoVietAnh2 <caovietanh2799@gmail.com>
2025-04-06 16:18:26 +0700

    Update settings.php

--- project2/settings.php
+++ project2/settings.php
@@ -1,7 +1,7 @@
 <?php
 $host = "feenix-mariadb.swin.edu.au";
 $user = "s105514373";
-$pwd = "230901";
+$pwd = "230901"; 
 $sql_db = "s105514373_db";
 
 $conn = new mysqli($host, $user, $pwd, $sql_db);
@@ -11,13 +11,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Enable detailed MySQL errors
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 try {
     $conn = new mysqli($host, $user, $pwd, $sql_db);
-    echo "Connected successfully!"; // Debugging
+    // Removed the echo statement here
 } catch (mysqli_sql_exception $e) {
     die("Database connection failed: " . $e->getMessage());
 }
-
 ?>
